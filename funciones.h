dc378a0e3dbf211667f3f19c68921abb6a04c707
José Manuel Serrano Sánchez <i22sesaj@uco.es>
2023-12-07 18:03:01 +0100

    Segundo commit de funciones.h

--- funciones.h
+++ funciones.h
@@ -8,7 +8,7 @@ void CreateAct(int act_count,std::list <Actividad> act_list);
 void ChangeAct(Actividad a);
 void AddInfo(Actividad a);
 void ShowAct(Actividad a);
-void HideAct(Actividad a)
+void HideAct(Actividad a);
 void SeeActs(int rol, std::list<Actividad> act_list);
 bool AddList(Actividad a,std::list <Actividad> act_list);
 bool DeleteList(Actividad a,std::list <Actividad> act_list);
