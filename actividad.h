ff6c8f691c4841ac0a509c971820e4b2c1210b08
José Manuel Serrano Sánchez <i22sesaj@uco.es>
2023-12-12 16:57:23 +0100

    Octavo commit de actividad.h

--- actividad.h
+++ actividad.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <string>
-#include <list>
 
 class Actividad{
 	private:
@@ -13,6 +12,7 @@ class Actividad{
 		std::string end_date_;
 		bool status_;
 		std::string description_;
+		float price_;
 		int capacity_;
 		int faculty_id_;
 
@@ -23,6 +23,7 @@ class Actividad{
 					std::string end_date="empty",
 					bool status=false,
 					std::string description="empty",
+					float price=0.0,
 					int capacity=0,
 					int faculty_id=0) {act_id_=act_id;}
 		
@@ -32,6 +33,7 @@ class Actividad{
 		std::string GetEndDate() {return end_date_;}
 		bool GetStatus() {return status_;}
 		std::string GetDescription() {return description_;}
+		float GetPrice() {return price_;}
 		int GetCapacity() {return capacity_;}
 		int GetFacultyId() {return faculty_id_;}
 
@@ -41,6 +43,7 @@ class Actividad{
 		void SetEndDate(std::string end_date) {end_date_=end_date;}
 		void SetStatus(bool status) {status_=status;}
 		void SetDescription(std::string description) {description_=description;}
+		void SetPrice(float price) {price_=price;}
 		void SetCapacity(int capacity) {capacity_=capacity;}
 		void SetFacultyId(int faculty_id) {faculty_id_=faculty_id;}
 };
