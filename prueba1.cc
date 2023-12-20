9fc4a6ddceebcbe76aea333648a6a8f3d017d38a
Equisde04 <152728697+Equisde04@users.noreply.github.com>
2023-12-20 18:46:11 +0100

    Add files via upload

--- prueba1.cc
+++ prueba1.cc
@@ -1,4 +1,6 @@
 #include <actividad.h>
+#include <preinscripcion.h>
+#include <inscripcion.h>
 #include <funciones.h>
 #include <string>
 #include <list>
@@ -52,4 +54,37 @@ TEST(TurnInt) {
     EXPECT_EQ(1, TurnInt(cad2));
 }
 
+TEST(Inscripcion, Añadir_inscripción){
+	std::list <Inscripcion> ins_list;
+	Inscripcion i(1);
+	EXPECT_EQ(true, AddListIns(i,ins_list));
+	Inscripcion a(1);
+	EXPECT_EQ(false, AddListIns(a,ins_list));
+}
 
+TEST(Inscripcion, Borrar_inscripción){
+	std::list <Inscripcion> ins_list;
+	Inscripcion i(1);
+	AddListIns(Inscripcion i,std::list <Inscripcion> &ins_list)
+	EXPECT_EQ(true, DeleteListIns(i,ins_list));
+	Inscripcion a(1);
+	EXPECT_EQ(false, DeleteListIns(a,ins_list));
+}
+
+TEST(Preinscripcion, Cambiar_estado){
+	std::list <Preinscripcion> pre_list;
+	Preinscripcion p1(1, 1, "User1", false, true);
+	Preinscripcion p2(2, 2, "User2", false, false);
+	AddListPre(p1,pre_list);
+	AddListPre(p2,pre_list);
+	EXPECT_EQ(true, ChangeStatus(p1.GetPreId(), pre_list));
+	EXPECT_EQ(false, ChangeStatus(p2.GetPreId(), pre_list));
+}
+
+TEST(Preinscripcion, Añadir_preinscripción){
+	std::list <Preinscripcion> pre_list;
+	Preinscripcion p(1);
+	EXPECT_EQ(true, AddListPre(p,pre_list));
+	Preinscripcion e(1);
+	EXPECT_EQ(false, AddListPre(e,pre_list));
+}
